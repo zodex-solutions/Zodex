82d49d8dc64339695e1799f13eab9830221e4357
abhaykoli1 <abhaykoli214@gmail.com>
2026-01-31 20:21:11 +0530

    C

--- ZodexBackend/config/db.js
+++ ZodexBackend/config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 const connectDB = async () => {
+  // New Upadaate
   try {
     await mongoose.connect(process.env.MONGO_URI);
     console.log("✅ MongoDB Connected");
